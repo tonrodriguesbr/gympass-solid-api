a2de49f174d75a05b0cf2cecb468d5ef2d21c261
Wellington Rodrigues <tonrodriguesbr@gmail.com>
2026-07-07 08:15:05 -0300

    feat: add Biome configuration files and update package dependencies

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,9 +2,7 @@ import "dotenv/config";
 import z from "zod";
 
 const envSchema = z.object({
-  NODE_ENV: z
-    .enum(["development", "production", "test"])
-    .default("development"),
+  NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
   PORT: z.coerce.number().default(3333),
   // DATABASE_URL: z.string(),
 });
